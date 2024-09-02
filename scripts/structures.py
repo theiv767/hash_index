4d28efd03f14a9a2296af03befe6584600897ff2
theiv767 <davib.g767@gmail.com>
2024-09-02 19:55:25 -0300

    CREATE hash structures

---
+++ scripts/structures.py
@@ -0,0 +1,107 @@
+# ESTRUTURAS DE DADOS: TUPLA, PÁGINA, TABELA, BUCKET
+
+
+
+class new_tuple:
+    def __init__(self, fields) -> None:
+        self.fields = fields
+
+    def get_field_value(self, field_name):
+        return self.fields[field_name]
+
+    def set_field_value(self, field_name, new_field_value) -> None:
+        self.fields[field_name] = new_field_value
+
+#-------------------------------------------------------------------
+
+class Page:
+    def __init__(self, identify) -> None:
+        self.id = identify
+        self.new_tuples = list()
+        pass
+
+    def add_new_tuple(self, new_tuple):
+        self.new_tuples.append(new_tuple)
+        pass
+
+#-------------------------------------------------------------------
+
+class Table:
+    def __init__(self, fields, primary_key, page_size=300) -> None:
+        self.primary_key = primary_key
+        self.field_names = fields
+        self.pages = list()
+        self.page_size = page_size
+        self.last_page = 0
+
+    def get_primary_key(self):
+        return self.primary_key
+
+    def get_record(self, primary_key, page_identify):
+        for memored_tuple in self.pages[page_identify]:
+            if memored_tuple[self.primary_key] == primary_key:
+                return memored_tuple
+
+        return None
+
+
+    def insert(self, new_tuple) -> bool:
+        #todo --> verificar se a primary_key está preenchida
+
+        new_tuple_field_names = list(new_tuple.keys())
+
+        for field in self.field_names:
+            if not (field in new_tuple_field_names):
+                new_tuple[field] = None
+                new_tuple_field_names.append(field)
+
+            if len(new_tuple_field_names) > len(self.field_names):
+                return False
+
+        if new_tuple[self.primary_key] == None:
+            return False
+
+
+        if len(self.pages) >= self.page_size:
+            new_page = Page(self.last_page+1)
+            self.last_page += 1
+            self.pages.append(new_page)
+
+
+        self.pages[self.last_page].append(new_tuple)
+
+
+        return True
+
+
+    def delete(self) -> bool:
+        pass
+
+#-------------------------------------------------------------------
+
+class Bucket:
+    def __init__(self, num_buckets, bucket_size, table_master) -> None:
+        self.table_master = table_master
+        self.num_buckets = num_buckets
+        self.bucket_size = bucket_size
+
+        self.buckets = list()
+        for i in range(num_buckets):
+            self.buckets[i] = list()
+
+    def add_value(self, tuple) -> bool:
+        value = tuple[self.table_master.get_primary_key()]
+        bucket_id = self.hash_function(value)
+        self.table_master.insert(tuple)
+
+        self.buckets[bucket_id] = (value, self.table_master.last_page)
+
+
+
+    def hash_function(input, normalize_max_value) -> int:
+        ascii_sum = sum(ord(char) ** 3 for char in input)
+
+        # normalização
+        hash_value = ascii_sum % normalize_max_value
+
+        return hash_value
